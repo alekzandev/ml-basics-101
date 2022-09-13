b5761b88bcf03e7a833359125236e2a548e39d50
And15015 <robotina00110011@gmail.com>
2022-09-12 22:04:45 -0500

    Update

--- x2_preprocessing_data.py
+++ x2_preprocessing_data.py
@@ -43,10 +43,10 @@ class PreprocessingData:
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.3, random_state=12
         )
-        X_train.to_csv("X_train.csv")
-        X_test.to_csv("X_test.csv")
-        y_train.to_csv("y_train.csv")
-        y_test.to_csv("y_test.csv")
+        X_train.to_csv("X_train.csv",index = False)
+        X_test.to_csv("X_test.csv", index = False)
+        y_train.to_csv("y_train.csv",index = False)
+        y_test.to_csv("y_test.csv", index = False)
 
 
 if __name__ == "__main__":
